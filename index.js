10df60b03bc70e474c051497bcbb358396b67620
Anthony FS <anthonyfs@Anthonys-MBP.attlocal.net>
2022-10-01 11:03:09 -0700

    added route for GET /products/all

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send("Home Page")
 })
 
+// curl -X GET -H "Page: 2" -H "Sort: 'price'" -H "Order: 'desc'" http://localhost:3000/products/all
+app.get('/products/all', (req, res) => {
+    const { page, sort, order } = req.headers
+    console.log(page)
+    res.send(`GET Products: ${page}, ${sort}, ${order}`)
+})
+
 app.listen(3000, () => {
     console.log('🚀 Server listening on port 3000')
 })
