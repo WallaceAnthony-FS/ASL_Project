c53962a4a56468d845ada6ed0236b70136c865be
Anthony FS <anthonyfs@Anthonys-MBP.attlocal.net>
2022-10-01 11:06:02 -0700

    Added route GET /products/:productId

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ app.get('/', (req, res) => {
 // curl -X GET -H "Page: 2" -H "Sort: 'price'" -H "Order: 'desc'" http://localhost:3000/products/all
 app.get('/products/all', (req, res) => {
     const { page, sort, order } = req.headers
-    console.log(page)
     res.send(`GET Products: ${page}, ${sort}, ${order}`)
+})
+
+// curl -X GET http://localhost:3000/products/8719
+app.get('/products/:productId', (req, res) => {
+    const { productId } = req.params
+    res.send(`GET Products: ${productId}`)
 })
 
 app.listen(3000, () => {
