47f8aec4403dd935aa7f694e8a401a29d2813264
Anthony FS <anthonyfs@Anthonys-MBP.attlocal.net>
2022-10-15 15:05:38 -0700

    Added slug to product

--- models/product.js
+++ models/product.js
@@ -16,7 +16,10 @@ module.exports = (sequelize, DataTypes) => {
   }
   Product.init({
     title: DataTypes.STRING,
-    price: DataTypes.INTEGER
+    slug: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    isPublished: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Product',
